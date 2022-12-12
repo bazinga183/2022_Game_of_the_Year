b9f3ed25b7fd209dcb19c06bf5a949c42d7c206e
Eduardo Flores <46951897+bazinga183@users.noreply.github.com>
2022-12-12 17:09:27 -0500

    Added SQL query for basic score analysis

--- goty_queries.sql
+++ goty_queries.sql
@@ -1,6 +1,7 @@
 CREATE TABLE goty (
 	index text,
 	game varchar(32),
+	console varchar(16),
 	username varchar(32),
 	user_score int,
 	review_date date,
@@ -9,5 +10,12 @@ CREATE TABLE goty (
 	total_thumb_ratings int
 );
 
-SELECT *
-FROM goty;
+SELECT 
+	game,
+	console, 
+	COUNT(user_score) AS "Total User Score Ratings",
+	ROUND(AVG(user_score), 2) AS "Average User Score",
+	ROUND(AVG(thumbs_up), 2) AS "Average Helpful Thumbs Up Per Comment",
+	ROUND(AVG(total_thumb_ratings), 2) AS "Average Thumbs Per Comment"
+FROM goty
+GROUP BY game, console;
